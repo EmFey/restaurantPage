79cfc4519a46692c21747a3fc7e7ac4d44a7895b
Fey <manufety15@gmail.com>
2023-10-17 13:31:22 +0200

    Added functionality to load pages

---
+++ src/restaurant.js
@@ -0,0 +1,24 @@
+const createRestaurantHomePage = () => {
+    const content = document.querySelector('#content');
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
+
+    //Image element
+    const image = document.createElement('img');
+    image.src = "dist/fine.jpeg";
+    image.height = "300";
+    pageContent.appendChild(image);
+    
+    //Headline element
+    const headline = document.createElement('h1');
+    headline.textContent = "FINE DINE WITH US";
+    pageContent.appendChild(headline);
+    
+    //copy element
+    const copy = document.createElement('p');
+    copy.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita fugit consequatur ipsa assumenda recusandae nesciunt, architecto quod dignissimos tempore asperiores dolores. Doloribus iste consectetur modi ducimus enim autem rem veritatis quisquam. Magni porro harum eveniet sint molestiae accusantium eum omnis, amet nesciunt id molestias ab praesentium nisi vitae illum accusamus incidunt vel minima rem cumque consequuntur atque perspiciatis nulla aspernatur. Eos, voluptas facilis quasi veritatis ratione expedita deserunt quia fugit aspernatur voluptatum, rerum consequatur cupiditate provident impedit, qui illo nulla sapiente minus assumenda? Placeat sit nam beatae qui illum dolores cupiditate, earum, et ipsum, ducimus blanditiis. Distinctio libero natus eius.";
+    pageContent.appendChild(copy);
+    content.appendChild(pageContent);
+}
+
+export default createRestaurantHomePage;
