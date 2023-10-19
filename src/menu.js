189080a990835fce4fc5122f560fc11bef58e53d
Fey <manufety15@gmail.com>
2023-10-19 14:29:16 +0200

    Styled the Menu page and edited readMe

--- src/menu.js
+++ src/menu.js
@@ -1,28 +1,84 @@
 const createMenuPage = () => {
     const content = document.querySelector('#content');
-const pageContent = document.createElement('div');
-pageContent.classList.add('page-content');
+    const pageContent = document.createElement('div');
+    pageContent.classList.add('page-content');
 
-const heading = document.createElement('h1');
-heading.textContent = 'Our Menu';
+    // First div - Appetizers
+    const appetizersDiv = document.createElement('div');
+    appetizersDiv.classList.add('menu-category');
+    const appetizersHeading = document.createElement('h2');
+    appetizersHeading.textContent = 'Appetizers';
 
-const menuList = document.createElement('ul');
+    const appetizersList = document.createElement('ul');
+    const appetizerItem1 = document.createElement('li');
+    appetizerItem1.textContent = "Pomodoro";
 
-const menuItem1 = document.createElement('li');
-menuItem1.textContent = "Pomodoro";
+    const appetizerItem2 = document.createElement('li');
+    appetizerItem2.textContent = "Bolognesa";
 
-const menuItem2 = document.createElement('li');
-menuItem2.textContent = "Bolognesa";
+    const appetizerItem3 = document.createElement('li');
+    appetizerItem3.textContent = "Garlic Bread";
 
-const menuItem3 = document.createElement('li');
-menuItem3.textContent = "Alfredo";
-menuList.appendChild(menuItem1);
-menuList.appendChild(menuItem2);
-menuList.appendChild(menuItem3);
+    appetizersList.appendChild(appetizerItem1);
+    appetizersList.appendChild(appetizerItem2);
+    appetizersList.appendChild(appetizerItem3);
 
-pageContent.appendChild(heading);
-pageContent.appendChild(menuList);
-content.appendChild(pageContent);
+    appetizersDiv.appendChild(appetizersHeading);
+    appetizersDiv.appendChild(appetizersList);
+
+    // Second div - Drinks
+    const drinksDiv = document.createElement('div');
+    drinksDiv.classList.add('menu-category');
+    const drinksHeading = document.createElement('h2');
+    drinksHeading.textContent = 'Drinks';
+
+    const drinksList = document.createElement('ul');
+    const drinkItem1 = document.createElement('li');
+    drinkItem1.textContent = "Soda";
+
+    const drinkItem2 = document.createElement('li');
+    drinkItem2.textContent = "Mocktail";
+
+    const drinkItem3 = document.createElement('li');
+    drinkItem3.textContent = "Iced Tea";
+
+    drinksList.appendChild(drinkItem1);
+    drinksList.appendChild(drinkItem2);
+    drinksList.appendChild(drinkItem3);
+
+    drinksDiv.appendChild(drinksHeading);
+    drinksDiv.appendChild(drinksList);
+
+    // Third div - Plates
+    const platesDiv = document.createElement('div');
+    platesDiv.classList.add('menu-category');
+    const platesHeading = document.createElement('h2');
+    platesHeading.textContent = 'Plates';
+
+    const platesList = document.createElement('ul');
+    const plateItem1 = document.createElement('li');
+    plateItem1.textContent = "Alfredo";
+
+    const plateItem2 = document.createElement('li');
+    plateItem2.textContent = "Carbonara";
+
+    const plateItem3 = document.createElement('li');
+    plateItem3.textContent = "Lasagna";
+
+    platesList.appendChild(plateItem1);
+    platesList.appendChild(plateItem2);
+    platesList.appendChild(plateItem3);
+
+    platesDiv.appendChild(platesHeading);
+    platesDiv.appendChild(platesList);
+
+    // Append the three divs to pageContent
+    pageContent.appendChild(appetizersDiv);
+    pageContent.appendChild(drinksDiv);
+    pageContent.appendChild(platesDiv);
+
+    // Append the pageContent to the content
+    content.appendChild(pageContent);
 };
 
 export default createMenuPage;
