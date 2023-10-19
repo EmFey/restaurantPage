3148d573b38cf45c05c9689d1ada6e93a7bea907
Fey <manufety15@gmail.com>
2023-10-19 14:06:19 +0200

    Made changes to the home page and styled it

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,21 +3,38 @@ const createRestaurantHomePage = () => {
     const pageContent = document.createElement('div');
     pageContent.classList.add('page-content');
 
-    //Headline element
+    // First div with headline and copy
+    const textDiv = document.createElement('div');
+
+    // Headline element
     const headline = document.createElement('h1');
     headline.textContent = "FINE DINE WITH US";
-    pageContent.appendChild(headline);
-    
-    //Image element
-    const image = document.createElement('img');
-    image.src = "fine.jpeg";
-    image.height = "300";
-    pageContent.appendChild(image);
-    
-    //copy element
+    textDiv.appendChild(headline);
+
+    // Copy element
     const copy = document.createElement('p');
     copy.textContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita fugit consequatur ipsa assumenda recusandae nesciunt, architecto quod dignissimos tempore asperiores dolores. Doloribus iste consectetur modi ducimus enim autem rem veritatis quisquam. Magni porro harum eveniet sint molestiae accusantium eum omnis, amet nesciunt id molestias ab praesentium nisi vitae illum accusamus incidunt vel minima rem cumque consequuntur atque perspiciatis nulla aspernatur. Eos, voluptas facilis quasi veritatis ratione expedita deserunt quia fugit aspernatur voluptatum, rerum consequatur cupiditate provident impedit, qui illo nulla sapiente minus assumenda? Placeat sit nam beatae qui illum dolores cupiditate, earum, et ipsum, ducimus blanditiis. Distinctio libero natus eius.";
-    pageContent.appendChild(copy);
+    textDiv.appendChild(copy);
+
+    // Append the first div to pageContent
+    pageContent.appendChild(textDiv);
+
+    // Second div with image
+    const imageDiv = document.createElement('div');
+
+    // Image element
+    const image = document.createElement('img');
+    image.src = "/home/fey/repos/restaurantPage/src/fine.jpeg";
+    image.alt = "Fine Dine Image";
+    image.height = "300";
+
+    // Append the image to the second div
+    imageDiv.appendChild(image);
+
+    // Append the second div to pageContent
+    pageContent.appendChild(imageDiv);
+
+    // Append the pageContent to the content
     content.appendChild(pageContent);
 };
 
